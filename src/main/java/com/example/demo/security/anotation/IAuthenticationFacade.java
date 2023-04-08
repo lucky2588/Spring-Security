3f26ff6e940e18896ed4f1f050be450c3e271cbe
lucky2588 <duwcsthang2001@gmail.com>
2023-04-08 17:55:30 +0700

    Create IAuthenticationFacade.java

---
+++ src/main/java/com/example/demo/security/anotation/IAuthenticationFacade.java
@@ -0,0 +1,9 @@
+package com.example.demo.security.anotation;
+
+import org.springframework.security.core.Authentication;
+
+public interface IAuthenticationFacade {
+    Authentication getAuthontication();
+
+
+}
